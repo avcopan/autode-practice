ba1d21d4fabfb2df6f93468943f8d654fcb13053
avcopan <avcopan@gmail.com>
2025-05-23 11:01:10 -0500

    New: Figures out how to run with multiple cores

---
+++ opt/h2_multi/run.py
@@ -0,0 +1,16 @@
+import autode as ade
+import os
+
+ade.Config.n_cores = 8
+
+print("n_cores:", ade.Config.n_cores)
+print("get_lmethod:", ade.methods.get_lmethod())
+print("get_hmethod:", ade.methods.get_hmethod())
+
+h2 = ade.Molecule(smiles='[H][H]')
+
+h2.optimise(method=ade.methods.get_lmethod())
+
+h2.optimise(method=ade.methods.get_hmethod())
+
+print(h2.energy)
